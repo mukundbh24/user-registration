9f3646dfd38780fa50d406f9bc81ff8ada7ab478
Mukund Bhosale <mukund.bhosale@kyndryl.com>
2023-12-25 13:20:24 +0530

    Created app.py for user registration backend

---
+++ app.py
@@ -0,0 +1,34 @@
+# app.py
+
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+bcrypt = Bcrypt(app)
+
+# Dummy user storage (replace this with a proper database)
+users = []
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = {'username': form.username.data, 'password': hashed_password}
+        users.append(user)
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
